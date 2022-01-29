60f1a81599456109abdd8c0182721bc64284bd8f
rrgraph <muhammadrizalwiyono@gmail.com>
2022-01-29 21:17:39 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ini Ceritanya Dibawah Ini Itu Searchnya 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" placeholder="Input your search..."/>
       </header>
     </div>
   );
