cb0a2cbc6628367434e7efbf4f9523e67ad16eca
Jay <leejungjay1124@gmail.com>
2022-01-30 13:43:16 +0700

    define api search query

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [ state, setState ] = useState({})
+  const [ query, setQuery ] = useState()
+
+  useEffect(() => {
+    setState({})
+    if (query) {
+      fetch(`https://api.agify.io?name=${query}`)
+      .then(response => response.json())
+      .then(result => setState(result))
+      .catch(error => console.error(error))
+    }
+  }, [query])
+
+  function searchQuery(evt) {
+    if (evt.key === 'Enter') {
+      const value = evt.target.value
+      setQuery(value)
+    }
+  } 
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ini Ceritanya Dibawah Ini Itu Searchnya 
+          Type any name, and get the age.
         </p>
-        <input type="text" placeholder="Input your search..."/>
+        <input type="text" onKeyPress={searchQuery} placeholder="Type name..."/>
+        { Object.keys(state).length !== 0 ? (<div>Name: {state.name}</div>) : (<></>)}
+        { Object.keys(state).length !== 0 ? (<div>Age : {state.age}</div>) : (<></>)}
       </header>
     </div>
   );
